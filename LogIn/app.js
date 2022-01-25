89a8d4adb1301ce2f4120126fc0e55307658f636
matiasnmoyano <moyano.matiasnicolas.mm@gmail.com>
2022-01-24 23:45:15 -0300

    spacing

--- LogIn/app.js
+++ LogIn/app.js
@@ -3,9 +3,11 @@ const bodyParser = require ("body-parser");
 const mongoose = require ("mongoose")
 const app = express()
 const Users = require("./routes")
+
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use("/logIn", Users)
+
 mongoose.connect('mongodb://localhost:27017/test')
 .then(() => {
     app.listen(3000, () => {
